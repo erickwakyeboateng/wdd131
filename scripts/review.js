a5bb903fefe0ff52953dde1d3285d58eab2077e9
Eric Boateng <boateng.eric44@gmail.com>
2026-04-01 18:16:09 +0000

    Product Review Form

---
+++ scripts/review.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let count = localStorage.getItem("reviewCount");
+
+  if (!count) {
+    count = 0;
+  }
+
+  count++;
+
+  localStorage.setItem("reviewCount", count);
+
+  document.getElementById("counter").textContent = count;
+});
